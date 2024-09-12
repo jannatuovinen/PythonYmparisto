903b520f9948ab4cfb69886070819d5f91439d2c
jannatuovinen <51450@edu.raseko.fi>
2024-09-12 14:29:41 +0300

    Lisätty pääohjelmaan ulkoiset kirjastot ja asetukset

---
+++ app.py
@@ -0,0 +1,13 @@
+# SOVELLUKSEN PÄÄOHJELMA
+# ======================
+
+# KIRJASTOT JA MODUULI
+# --------------------
+
+# OpenCV-kirjasto videokameraa varten
+import cv2
+
+# ASETUKSET
+# ---------
+
+kameraIndeksi = 1 # Ensimmäinen kamera on aina 0
