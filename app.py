f0c32785d1c06a8244a6d515ca623f545f1ea9e7
jannatuovinen <51450@edu.raseko.fi>
2024-09-12 15:13:26 +0300

    Lisätty piippausäänikoodi

--- app.py
+++ app.py
@@ -5,9 +5,23 @@
 # --------------------
 
 # OpenCV-kirjasto videokameraa varten
-import cv2
+# import cv2
+
+# Winsound-kirjasto äänikorttia varten
+import winsound
 
 # ASETUKSET
 # ---------
 
 kameraIndeksi = 1 # Ensimmäinen kamera on aina 0
+
+# Funktiot
+
+def piippaa():
+    """Tuottaa puolen sekunnin 1 kHz äänimerkin
+    """
+    taajuus = 1000 # Hz
+    kesto = 500 # ms
+    winsound.Beep(taajuus, kesto)
+
+piippaa()
