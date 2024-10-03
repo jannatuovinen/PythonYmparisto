fcbb47c744fc8c91e2b184c4f46ff5e639fc6786
jannatuovinen <51450@edu.raseko.fi>
2024-10-03 12:51:29 +0300

    Refaktointi: korjattu päivätarkistus pituustarkistuksen sisälle

--- identityCheck.py
+++ identityCheck.py
@@ -45,10 +45,39 @@ def checkHeTu(hetu):
     # Lasketaan HeTu-parametrin pituus
     length = len(hetu)
 
+    # Jos pituus on oikea tehdään eri osat
+    if length == 11:
+        dayPart = hetu[0:2]
+        monthPart = hetu[2:4]
+        yearPart = hetu[4:6]
+        centuryPart = hetu[6:7]
+        numberPart = hetu[7:10]
+        checkSum = hetu[10]
+        
+        # Tarkistetaan päiväosan oikeellisuus, pitää olla pelkkiä numeroita
+        if dayPart.isdigit():
+            day = int(dayPart)
+
+            # Päivän pitää olla väliltä 1 - 31
+            if day < 1:
+                result = (3, 'Päivä virheellinen')
+            if day > 31:
+                result = (3, 'Päivä virheellinen')
+        else:
+            # Jos muuta, kuin pelkkiä numeroita
+            result = (3, 'Päivä virheellinen')
+
+        
     if length < 11:
         result = (1, 'Henkilötunnus liian lyhyt')
 
     if length > 11:
         result = (2, 'Henkilötunnus liian pitkä')
-        
     return result
+
+if __name__ == "__main__":
+    hetu = '130728-478N'
+    paivat = hetu[0:2]
+    kuukaudet = hetu[2:4]
+    print(paivat)
+    print(kuukaudet)
