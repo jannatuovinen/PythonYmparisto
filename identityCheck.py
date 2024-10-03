6427bd4894caccc0a14db6d05ee1af5cee92a786
jannatuovinen <51450@edu.raseko.fi>
2024-10-03 10:29:01 +0300

    Lisätty koodia

--- identityCheck.py
+++ identityCheck.py
@@ -34,3 +34,21 @@ def opiskelijanumeroOk(opiskelijanumero: str) -> bool:
 # 2. Syntymäaikaosan oikeellisuus
 # 3. Vuosisatkoodit +, - ja A
 # 4. Modulo 31 tarkistus
+
+# Lopullisena tavoitteena on funktio, joka tarkistaa henkilötunnuksen oikeellisuuden ja palauttaa virhekoodin ja virheilmoituksen, joka kertoo mikä vika HeTu:ssa on. Esim 0, OK tai 1, tunnus liian lyhyt tai 3, tunnus liian pitkä jne.
+
+def checkHeTu(hetu):
+
+    # Oletustulos 0 OK jos kaikki on kunnossa
+    result = (0, 'OK')
+
+    # Lasketaan HeTu-parametrin pituus
+    length = len(hetu)
+
+    if length < 11:
+        result = (1, 'Henkilötunnus liian lyhyt')
+
+    if length > 11:
+        result = (2, 'Henkilötunnus liian pitkä')
+        
+    return result
--- test_identityCheck.py
+++ test_identityCheck.py
@@ -22,6 +22,44 @@ def test_opiskelijanumeroOk_7():
 def test_opiskelijanumeroOk_kirjain():
     assert identityCheck.opiskelijanumeroOk('123X45') == False
 
-# Joukossa desimaali -> väärin
+# Joukossa desimaalipiste -> väärin
 def test_opiskelijanumeroOk_desimaali():
     assert identityCheck.opiskelijanumeroOk('12.45') == False
+
+# Joukossa desimaalipilkku
+def test_opiskelijanumeroOk_desimaali2():
+    assert identityCheck.opiskelijanumeroOk('12,45') == False
+
+# TDD-TESTAUSTA
+# -------------
+
+
+def test_checkHeTuOK():
+    assert identityCheck.checkHeTu('130728-478N') == (0, 'OK')
+
+def test_checkHeTuShort():
+    assert identityCheck.checkHeTu('13028-478N') == (1, 'Henkilötunnus liian lyhyt')
+
+# Henkilötunnuksessa pitää olla 11 merkkiä, merkkejä on liikaa
+def test_checkHeTuLong():
+    assert identityCheck.checkHeTu('1307288-478N') == (2, 'Henkilötunnus liian pitkä')
+
+# Henkilötunnuksen päiväosassa saa olla 01 - 31
+def test_checkHeTuDays():
+    assert identityCheck.checkHeTu('450728-478N') == (3, 'Päivä virheellinen')
+
+# Henkilötunnuksen kuukausiosassa saa olla 01 - 12
+def test_checkHeTuMonths():
+    assert identityCheck.checkHeTu('132728-478N') == (4, 'Kuukausi virheellinen')
+
+# Henkilötunnuksen vuosiosassa saa olla 00 - 99
+def test_checkHetuYears():
+    assert identityCheck.checkHeTu('13072x-478N') == (5, 'Vuosi virheellinen')
+
+# Käytössä olevat vuosisatakoodit + (1800), - (1900) ja A (2000)
+def test_checkHetuCenturyCode():
+    assert identityCheck.checkHeTu('130728s478N') == (6, 'Vuosisatakoodi virheellinen')
+
+# Henkilötunnuksen numeroista tehdään luku, esim. 130728478 ja jaetaan se luvulla 31. Jakojäännös on tarkiste. Jos on alle 10, käytetään numeroa, jos yli haetaan taulukosta vastaava kirjanmerkki 10 -> A, 11-> B. G ja I eivät ole käytössä.
+def test_checkHeTuModulo():
+    assert identityCheck.checkHeTu('130728-478M') == (7, 'Varmistussumma ei täsmää')
