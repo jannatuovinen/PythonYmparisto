d9f82170258f43203c6a343f60caeaacfb495a29
jannatuovinen <51450@edu.raseko.fi>
2024-10-28 09:40:08 +0200

    Lisätty tehtäviä asioita TODO-lista

--- app.py
+++ app.py
@@ -7,19 +7,30 @@
 # MODUULIT
 # --------
 
-import sound # Äänimerkkimoduuli
-import video # Videomoduuli
+from avtools import sound # Äänimerkkimoduuli
+# from avtools import video # Videomoduuli
+import identityCheck2
 
 # ASETUKSET
 # ---------
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
-# Käynnistetään videokuva ja ilmoitetaan sen käynnistymisestä äänimerkillä
-sound.parametricBeep(400, 330)
-sound.playWav('Alkaa.WAV')
+# TODO: Pääohjelman ikuinen silmukka, josta poistutaan tarvittaessa (keksi menismi itse)
+# TODO: Paranna pääohjelmaa siten, että se ei kaadu, kun käyttäjä syöttää virheellisen henkilötunnuksen
+userGivensSsn = input('Syötä asiakkaan henkilötunnus: ')
+userGivenLastName = input('Syötä asikkaan sukunimi')
+# Tee tarkistus siitä, että nimi ei voi olla tyhjä
+userGivenFirstName = input('Syötä asikkaan etunimi')
+# TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
+# TODO: Varaudu toöamteeseen, jossa hetu:n tarkiste on annettu pienillä kirjaimilla
+# TODO: Muuta syötetty nimien alkukirjain isoksi
 
-# TESTIT KOODAUKSEN AIKANA
-# ========================
+ssnToCheck = identityCheck2.NationalSSN(userGivensSsn)
+if ssnToCheck.isValidSsn() == True:
+    dateOfBirth = ssnToCheck.getDateOfBirth
+    gender = ssnToCheck.getGender()
+    age = ssnToCheck.calculateAge()
+    print('Syntymäaika', ssnToCheck.dateOfBirth)
+    print('Ikä:', age)
+    print('Sukupuoli:', ssnToCheck.gender)
 
-if __name__ == "__main__":
-    pass
