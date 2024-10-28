241edbdbce7a3e6c300b20b0a2bb18d0bd227ed7
jannatuovinen <51450@edu.raseko.fi>
2024-10-28 12:55:30 +0200

    26.10 asioita

--- app.py
+++ app.py
@@ -15,41 +15,79 @@ import identityCheck2
 # ---------
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
+# FUNKTIOT
+# --------
 
+def askName(question: str) -> str:
+    """Promts user to answer a question and converts the answers to title and removes white space
+
+    Args:
+        question (str): Promt to user
+
+    Returns:
+        str: modifies answer
+    """
+    name = ''
+    while name == '':
+        name = input(question).strip()
+    name = name.title()
+    return name
+
+# PÄÄOHJELMAN IKUINEN SILMUKKA
+# ============================
 while True:
+
+    # Alustetaan muuttujat tyhjiksi
+    userGivensSsn = ''
+    userGivenLastName = ''
+    userGivenFirstName = ''
    
+   # Kysytään asiakkaan henkilötunnus ja muutetaan kirjaimet isoksi
     userGivensSsn = input('Syötä asiakkaan henkilötunnus: ')
     userGivensSsn = userGivensSsn.upper() # Varmistetaan, että tarkiste on isolla
-   
-    # TODO; Tee tarkistus siitä, että nimi ei voi olla tyhjä
     
-    # TODO: Rakenna funktio, jolla kysytään nimet ja muutetaan yhdysnimet isoille alkukirjaimille -> reg exp
+    # TODO: Rakenna funktio, jolla kysytään nimet ja muutetaan yhdysnimet isoille alkukirjaimille -> .title()
    
 
     
-    
+    # Luodaan syötetystä henkilötunnuksesta NationalSSN-objekti
     ssnToCheck = identityCheck2.NationalSSN(userGivensSsn)
+
+    # Tarkistetaan onko henkilötunnus oikein muodostettu
     if ssnToCheck.isValidSsn() == True:
+
+        # Virheenkäsittely, mahdollisen vuosisatakoodivirheen varalta
         try:
             ssnToCheck.getDateOfBirth
             ssnToCheck.getGender()
             age = ssnToCheck.calculateAge()
-            userGivenLastName = input('Syötä asikkaan sukunimi: ')
-            userGivenLastName = userGivenLastName.capitalize()
-            userGivenFirstName = input('Syötä asikkaan etunimi: ')
-            userGivenFirstName = userGivenFirstName.capitalize()
+
+            # Kysytään loput tiedot, jos ei virhettä
+            while userGivenLastName == '':
+                userGivenLastName = input('Syötä asikkaan sukunimi: ')
+            userGivenLastName = userGivenLastName.title()
+            while userGivenFirstName == '':
+                userGivenFirstName = input('Syötä asikkaan etunimi: ')
+            userGivenFirstName = userGivenFirstName.title()
+
+            # Tulostetaan tiedot ruudulle
             print('Asiakas:', userGivenLastName, userGivenFirstName)
             print('Syntymäaika', ssnToCheck.dateOfBirth)
             print('Ikä:', age)
             print('Sukupuoli:', ssnToCheck.gender)
+        
+        # Virhetilanteessa näytetään virheilmoitus
         except Exception as e:
             print('Syöttämässäsi sosiaaliturvatunnuksessa oli virhe', e)
         
     # TODO: Lisää tähän else-haara, joka kertoo, että HeTu oli virheellinen
 
     # Kysy halutaanko poistua ohjelmasta
-    wantAbort = input('Haluatko päättää ohjelman k/E: ')
+    wantToAbort = input('Haluatko päättää ohjelman k/E: ')
     # Muutetaan vastaus isoiksi kirjaimiksi ja tarkistetaan onko vastaus K
-    if wantAbort.upper() == 'K':
+    if wantToAbort.upper() == 'K':
         break # Poistutaan ikuisesta silmukasta
+
+if __name__ == "__main__":
+    askName('Anna etunimi')
 
