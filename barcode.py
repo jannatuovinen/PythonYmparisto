af002f298660a224fb694c44c232b3d77ac2e05a
jannatuovinen <156067718+jannatuovinen@users.noreply.github.com>
2024-10-31 13:09:23 +0200

    31.10 aamupäivä

--- barcode.py
+++ barcode.py
@@ -32,19 +32,29 @@ def calculateCode128BChecksum(text: str) -> int:
     Returns:
         int: Modulo 103 checksum weighted values
     """
-    text = text.strip()
+    text = text.strip() # Poistetaan ylimääräiset tyhjät alusta ja lopusta
     numberOfLetters = len(text)
-    weightedSum = 0
+    weightedSum = 0 # Alustetaan summa tyhjäksi
+
+    # Käydään teksti kirjaimittain läpi
     for number in range(numberOfLetters):
         letter = text[number]
+
+        # Kutsutaan funktiota, joka palauttaa 128-koodin arvon
         code128BValue = barCodeValue(letter)
+
+        # Lasketaan sijannilla painotettu painotettu arvo
         weightedValue = code128BValue * (number + 1)
+
+        # Lisätään se summaan
         weightedSum = weightedSum + weightedValue
-    weightedSum = weightedSum + 104
-    code128BChecksum = weightedSum % 103
+
+    # Lisätään alkumerkin arvo silmukan jälkeen
+    weightedSum = weightedSum + 104 # Lisätään alkumerkin arvo
+    code128BChecksum = weightedSum % 103 # Lasketaan jakojäännös mod 103
     return code128BChecksum
 
-# TODO: Tee tämä funktio loppuun
+# TODO: Tee tämä funktio loppuun ja testaa sitä notepadissa
 def createCode128B(text: str) -> str:
     """Creates a complete code128B barcode to be printed using Libre Code 128 font
 
@@ -54,10 +64,15 @@ def createCode128B(text: str) -> str:
     Returns:
         str: String containing start, barcode, checksum and stop symbols
     """
-    
-    code128Barcodestring = ''
-    return code128Barcodestring
+    code128BarcodeString = ''
+    startChar = chr(204)
+    stopChar = chr(206)
+    checkSum = calculateCode128BChecksum(text)
+    checkSumSymbol = chr(checkSum + 32)
+    code128BarcodeString = startChar + text + checkSumSymbol + stopChar
+    return code128BarcodeString
 
 if __name__ == "__main__":
     testString = '128B'
     print('painoteut arvot yhteensä:', calculateCode128BChecksum(testString))
+    print('Koko viivakoodi on', createCode128B('128B'))
