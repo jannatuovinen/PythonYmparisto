d5be4b7fd39246253433f41a448aaec81d55db46
jannatuovinen <156067718+jannatuovinen@users.noreply.github.com>
2024-11-19 11:25:34 +0200

    19.11 muutokset

---
+++ UiFromConvertedLabra.py
@@ -0,0 +1,78 @@
+# LABORATORIOETIKETTISOVELLUKSEN PÄÄIKKUNAN
+# LUOMINEN Labra_ui.py TIEDOSTON PERUSTEELLA
+# ==========================================
+
+# KIRJASTON JA MODUULIEN LATAUKSET
+# --------------------------------
+import os # Polkumääritykset
+import sys # Käynnistysargumentit
+
+from PySide6 import QtWidgets # Qt-vimpaimet
+from Labra_ui import Ui_MainWindow # Käännetyn käyttöliittymän luokka
+
+import identityCheck2 # Henkilötunnuksen tarkistukseen liittyvät työkalut
+import barcode # Viivakoodin muodostukseen tarvittavat rutiinit
+from avtools import sound # Äänitoiminnot
+
+# Määritellään luokka, joka perii QMainWindow- ja Ui_MainWindow-luokan
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    """A class for creating main window for the application"""
+
+    # Määritellään olionmuodostin ja kutsutaan yliluokkien muodostimia
+    def __init__(self):
+        super().__init__()
+
+        # Luodaan käyttöliittymä konvertoidun tiedoston perusteella MainWindow ui-ominaisuudeksi. Tämä suojaa lopun MainWindow-olion ylikirjoituksella
+        self.ui = Ui_MainWindow()
+
+        # Kutsutaan käyttöliittymän muodostusmetodia setupUi
+        self.ui.setupUi(self)
+
+        # OHJELMOIDUT SIGNAALIT
+        # ---------------------
+
+        # Kun poistutaan ssnLineEdit-elemntistä suoritetaan barcodeLabel-elementin päivitys
+        self.ui.ssnLineEdit.editingFinished.connect(self.updateBarcodeLabel)
+
+    
+    
+    # OHJELMOIDUT SLOTIT
+    # ------------------
+
+    # Viivakoodin muodostus ja barcodeLabel:n päivitys
+    def updateBarcodeLabel(self):
+        # Tarkistetaan, että henkilötunnus on oikein muodostettu
+        uiSsn = self.ui.ssnLineEdit.text().upper() # Luetaan käyttöliittymästä henkilötunnus
+        ssnToCheck = identityCheck2.NationalSSN(uiSsn) # Luodaan henkilötunnusobjekti
+        
+        # Jos se on oikein, luodaan viivakoodi
+        if ssnToCheck.isValidSsn():
+            barcode128 = barcode.Code128B(uiSsn) # Luodaan viivakoodi-olio
+            barcodeToPrint = barcode128.buildBarcode() # Lisätään alku- ja loppumerkki sekä varmistussumma
+            self.ui.barcodeLabel.setText(barcodeToPrint) # Päivitetään käyttöliittymän
+
+        # Jos se muodostettu väärin näytetään virheilmoitus MessageBox-ikkunassa
+        else:
+            self.errorTitle = 'Henkilötunnus virheellinen'
+            self.errorText = 'Syöttämässäsi henkilötunnuksessa on virhe'
+            self.openErrorMsgBox(self.errorTitle, self.errorText)
+
+    # Virheilmoitusikkuna
+    def openErrorMsgBox(self, errorTitle, errorText):
+        msgBox = QtWidgets.QMessageBox()
+        msgBox.setIcon(QtWidgets.QMessageBox.Critical)
+        msgBox.setWindowTitle(errorTitle)
+        msgBox.setText(errorText)
+        msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msgBox.exec()
+
+if __name__ == "__main__":
+
+    # Luodaan sovellus ja käynnistetään se
+    app = QtWidgets.QApplication(sys.argv)
+
+    # Luodaan objekti pääikkunalle ja tehdään siitä näkyvä
+    window = MainWindow()
+    window.show()
+
+    app.exec()
